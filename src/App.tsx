1d881a24f680fa4ff92b98d4979e20529f6a5e13
MCTBL <2772337924@qq.com>
2025-11-26 17:18:21 +0800

    重构 App 组件，优化侧边栏菜单结构并更新样式

--- src/App.tsx
+++ src/App.tsx
@@ -1,82 +1,79 @@
 import {
-	MenuFoldOutlined,
-	MenuUnfoldOutlined,
-	UploadOutlined,
+	LaptopOutlined,
+	NotificationOutlined,
 	UserOutlined,
-	VideoCameraOutlined,
 } from "@ant-design/icons";
-import { Button, Layout, Menu, theme } from "antd";
-import { Content, Header } from "antd/es/layout/layout";
-import React, { useState } from "react";
+import type { MenuProps } from "antd";
+import { Layout, Menu, theme } from "antd";
+import React from "react";
 
-const { Sider } = Layout;
+const { Header, Content, Sider } = Layout;
+
+const items2: MenuProps["items"] = [
+    UserOutlined,
+    LaptopOutlined,
+    NotificationOutlined,
+].map((icon, index) => {
+    const key = String(index + 1);
+
+    return {
+        key: `sub${key}`,
+        icon: React.createElement(icon),
+        label: `subnav ${key}`,
+        children: Array.from({ length: 4 }).map((_, j) => {
+            const subKey = index * 4 + j + 1;
+            return {
+                key: subKey,
+                label: `option${subKey}`,
+            };
+        }),
+    };
+});
 
 const App: React.FC = () => {
-	const [collapsed, setCollapsed] = useState(false);
-	const {
-		token: { colorBgContainer, borderRadiusLG },
-	} = theme.useToken();
+    const {
+        token: { colorBgContainer, borderRadiusLG },
+    } = theme.useToken();
 
-	return (
-		<Layout style={{ height: "100vh", width: "100%" }}>
-			<Sider trigger={null} collapsible collapsed={collapsed}>
-				<div className="demo-logo-vertical" />
-				<Menu
-					theme="dark"
-					mode="inline"
-					defaultSelectedKeys={["1"]}
-					items={[
-						{
-							key: "1",
-							icon: <UserOutlined />,
-							label: "nav 1",
-						},
-						{
-							key: "2",
-							icon: <VideoCameraOutlined />,
-							label: "nav 2",
-						},
-						{
-							key: "3",
-							icon: <UploadOutlined />,
-							label: "nav 3",
-						},
-					]}
-				/>
-			</Sider>
-			<Layout>
-				<Header style={{ padding: 0, background: colorBgContainer }}>
-					<Button
-						type="text"
-						icon={
-							collapsed ? (
-								<MenuUnfoldOutlined />
-							) : (
-								<MenuFoldOutlined />
-							)
-						}
-						onClick={() => setCollapsed(!collapsed)}
-						style={{
-							// fontSize: "16px",
-							width: 64,
-							height: 64,
-						}}
-					/>
-				</Header>
-				<Content
-					style={{
-						margin: "24px 16px",
-						padding: 24,
-						minHeight: 280,
-						background: colorBgContainer,
-						borderRadius: borderRadiusLG,
-					}}
-				>
-					Content
-				</Content>
-			</Layout>
-		</Layout>
-	);
+    return (
+        <Layout style={{ height: "100%" }}>
+            <Header style={{ display: "flex", alignItems: "center" }}>
+                <div className="demo-logo" />
+            </Header>
+            <Layout>
+                <Sider width={"10%"} style={{ background: colorBgContainer }}>
+                    <Menu
+                        mode="inline"
+                        defaultSelectedKeys={["1"]}
+                        defaultOpenKeys={["sub1"]}
+                        style={{ height: "100%", borderInlineEnd: 0 }}
+                        items={items2}
+                    />
+                </Sider>
+                <Layout style={{ padding: "24px 24px 24px 24px" }}>
+                    {/* <Breadcrumb
+                        items={[
+                            { title: "Home" },
+                            { title: "List" },
+                            { title: "App" },
+                        ]}
+                        style={{ margin: "16px 0" }}
+                    /> */}
+                    <Content
+                        style={{
+                            padding: 24,
+                            margin: 0,
+                            minHeight: 280,
+                            background: colorBgContainer,
+                            borderRadius: borderRadiusLG,
+                        }}
+                    >
+                        Content
+                    </Content>
+                </Layout>
+            </Layout>
+        </Layout>
+    );
 };
 
 export default App;
