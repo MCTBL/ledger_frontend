239399e7faa83d75e4173af2ca64a3115c90b965
MCTBL <2772337924@qq.com>
2026-03-11 15:31:08 +0800

    finish CRUD for bills

--- src/pages/BillsPage.tsx
+++ src/pages/BillsPage.tsx
@@ -5,6 +5,7 @@ import {
     Flex,
     Input,
     InputNumber,
+    message,
     Modal,
     Popconfirm,
     Space,
@@ -12,10 +13,11 @@ import {
     type TableColumnsType,
 } from "antd";
 import dayjs from "dayjs";
-import { useEffect, useRef, useState, type SyntheticEvent } from "react";
+import { useEffect, useState, type SyntheticEvent } from "react";
 import axios from "../api/axios";
 import { useAuth } from "../hooks/useAuth";
 import type {
+    Bill,
     billTableData,
     Category,
     getBillsData,
@@ -24,7 +26,7 @@ import type {
 
 export default function BillsPage() {
     const [data, setData] = useState<billTableData[]>([]);
-    const isFetched = useRef(false);
+    const [isFetched, setFetched] = useState(false);
     const [cateFilters, setCateFilters] = useState<
         { text: string; value: string }[]
     >([]);
@@ -32,6 +34,7 @@ export default function BillsPage() {
     const [updateData, setUpdataData] = useState({} as billTableData);
     const [allCategory, setAllCategory] = useState<string[]>([]);
     const [editOrNew, setEditOrNew] = useState<boolean>(false);
+    const [messageApi, contextHolder] = message.useMessage();
 
     const columns: TableColumnsType<billTableData> = [
         {
@@ -40,8 +43,7 @@ export default function BillsPage() {
             key: "date",
             align: "center",
             width: "20%",
-            sorter: (a, b) =>
-                new Date(a.date).getTime() - new Date(b.date).getTime(),
+            sorter: (a, b) => dayjs(a.date).valueOf() - dayjs(b.date).valueOf(),
         },
         {
             title: "分类",
@@ -110,14 +112,93 @@ export default function BillsPage() {
     }
 
     const handleDelete = (id: number) => {
-        // TODO
-        console.log(id);
+        axios
+            .delete(`/api/bills/delete/${user!.id}/${id}`)
+            .then((res) => {
+                const d = res.data as Result<string>;
+                if (d.code != 200) throw d.message;
+                setFetched(false);
+                messageApi.open({
+                    type: "success",
+                    content: "删除成功",
+                });
+            })
+            .catch((res) => {
+                messageApi.open({
+                    type: "error",
+                    content: `删除失败 ${res}`,
+                });
+            });
     };
 
     const handleSubmit = (temp: SyntheticEvent) => {
         temp.nativeEvent.preventDefault();
         console.log(updateData);
-        // TODO
+        if (updateData.amount === 0) {
+            messageApi.open({
+                type: "error",
+                content: `金额不可为 0`,
+            });
+        } else if (
+            updateData.category == undefined ||
+            updateData.category.length == 0
+        ) {
+            messageApi.open({
+                type: "error",
+                content: `类别不可为空`,
+            });
+        } else {
+            const newBill = {
+                id: updateData.id,
+                userId: user!.id,
+                categoryId: 0,
+                amount: Math.abs(Number(updateData.amount)),
+                consume: updateData.amount > 0 ? false : true,
+                billDate: updateData.date,
+                billDescription: updateData.description,
+            } as Bill;
+            console.log(newBill);
+
+            if (editOrNew) {
+                axios
+                    .post(`/api/bills/update/${updateData.category}`, newBill)
+                    .then((response) => {
+                        const d = response.data as Result<string>;
+                        if (d.code != 200) throw d.message;
+                        setFetched(false);
+                        closeEditModal();
+                        messageApi.open({
+                            type: "success",
+                            content: `账单修改成功`,
+                        });
+                    })
+                    .catch((res) => {
+                        messageApi.open({
+                            type: "error",
+                            content: `修改失败 ${res}`,
+                        });
+                    });
+            } else {
+                axios
+                    .post(`/api/bills/add/${updateData.category}`, newBill)
+                    .then((response) => {
+                        const d = response.data as Result<string>;
+                        if (d.code != 200) throw d.message;
+                        setFetched(false);
+                        closeEditModal();
+                        messageApi.open({
+                            type: "success",
+                            content: `账单添加成功`,
+                        });
+                    })
+                    .catch((res) => {
+                        messageApi.open({
+                            type: "error",
+                            content: `添加失败 ${res}`,
+                        });
+                    });
+            }
+        }
     };
 
     const getOptions = () =>
@@ -132,7 +213,7 @@ export default function BillsPage() {
         const newState = { ...updateData };
         switch (which) {
             case "date":
-                newState.date = newValue as string;
+                newState.date = dayjs(newValue).format("YYYY-MM-DD HH:mm:ss");
                 break;
             case "category":
                 newState.category = newValue as string;
@@ -150,8 +231,8 @@ export default function BillsPage() {
     const { user } = useAuth();
 
     useEffect(() => {
-        if (isFetched.current) return;
-        isFetched.current = true;
+        if (isFetched) return;
+        setFetched(true);
 
         axios
             .get(`/api/bills/get/${user!.id}`)
@@ -160,6 +241,7 @@ export default function BillsPage() {
                     .data as getBillsData;
                 const categoryList: Category[] = data.categories;
                 const bills = data.bills;
+                console.log(bills);
 
                 const categoriesMap = new Map<number, string>();
                 categoryList.forEach((cate) => {
@@ -181,9 +263,7 @@ export default function BillsPage() {
                 const tableData: billTableData[] = bills.map((bill) => ({
                     key: bill.id,
                     id: bill.id,
-                    date: new Date(bill.billDate).toLocaleString("zh-CN", {
-                        hour12: false,
-                    }),
+                    date: bill.billDate,
                     category: categoriesMap.get(bill.categoryId) || "",
                     amount: bill.consume ? -bill.amount : bill.amount,
                     description: bill.billDescription,
@@ -193,108 +273,127 @@ export default function BillsPage() {
             .catch((error) => {
                 console.log(error);
             });
-    }, [user]);
+    }, [user, isFetched]);
 
     return (
-        <Flex gap="middle" vertical style={{ height: "100%" }}>
-            <Flex flex={1} gap="large" align="center" justify="left">
-                <h3>账单详情</h3>
-                <Space>
-                    <Button
-                        type="primary"
-                        onClick={() => {
-                            openEditModal(
-                                {
-                                    key: 0,
-                                    id: 0,
-                                    date: dayjs().format("YYYY-MM-DD HH:mm:ss"),
-                                    category: "",
-                                    amount: 0,
-                                    description: "",
-                                },
-                                false,
-                            );
-                        }}
-                    >
-                        添加账单
-                    </Button>
-                </Space>
-            </Flex>
-            <Flex flex={15} justify="center">
-                <Table<billTableData>
-                    columns={columns}
-                    dataSource={data}
-                    style={{ width: "90%", height: "100%" }}
-                />
-            </Flex>
-            <Modal
-                title={editOrNew ? "编辑" : "新增"}
-                open={isModalOpen}
-                centered={true}
-                onCancel={closeEditModal}
-                onOk={handleSubmit}
-            >
-                <Flex vertical gap="middle">
-                    <Flex gap="middle">
-                        <Flex flex={1} align="center" justify="center">
-                            <Flex flex={3}>日期：</Flex>
-                            <DatePicker
-                                value={dayjs(updateData.date)}
-                                showTime={{ format: "HH:mm:ss" }}
-                                format="YYYY-MM-DD HH:mm:ss"
-                                onChange={(_, value) =>
-                                    handleUpdataDataChange(
-                                        "date",
-                                        value as string,
-                                    )
-                                }
-                                style={{ flex: 8 }}
-                            ></DatePicker>
-                        </Flex>
-                        <Flex flex={1} align="center" justify="center">
-                            <Flex flex={2}>类别：</Flex>
-                            <AutoComplete
-                                style={{ flex: 8 }}
-                                onChange={(value) =>
-                                    handleUpdataDataChange("category", value)
-                                }
-                                options={getOptions()}
-                                allowClear={true}
-                                backfill={true}
-                                value={updateData.category}
-                            />
-                        </Flex>
-                    </Flex>
-                    <Flex gap="middle">
-                        <Flex flex={1} align="center" justify="center">
-                            <Flex flex={3}>金额：</Flex>
-                            <InputNumber
-                                suffix="￥"
-                                value={updateData.amount}
-                                onChange={(value) =>
-                                    handleUpdataDataChange("amount", value)
-                                }
-                                precision={3}
-                                style={{ flex: 8 }}
-                            ></InputNumber>
-                        </Flex>
-                        <Flex flex={1} align="center" justify="center">
-                            <Flex flex={3}>备注：</Flex>
-                            <Input
-                                style={{ flex: 8 }}
-                                onChange={(element) =>
-                                    handleUpdataDataChange(
-                                        "description",
-                                        element.target.value,
-                                    )
-                                }
-                                allowClear={true}
-                                value={updateData.description}
-                            />
-                        </Flex>
-                    </Flex>
+        <>
+            {contextHolder}
+            <Flex gap="middle" vertical style={{ height: "100%" }}>
+                <Flex flex={1} gap="large" align="center" justify="left">
+                    <h3>账单详情</h3>
+                    <Space>
+                        <Button
+                            type="primary"
+                            onClick={() => {
+                                openEditModal(
+                                    {
+                                        key: 0,
+                                        id: 0,
+                                        date: dayjs().format(
+                                            "YYYY-MM-DD HH:mm:ss",
+                                        ),
+                                        category: "",
+                                        amount: 0,
+                                        description: "",
+                                    },
+                                    false,
+                                );
+                            }}
+                        >
+                            添加账单
+                        </Button>
+                    </Space>
                 </Flex>
-            </Modal>
-        </Flex>
+                <Flex flex={15} justify="center">
+                    <Table<billTableData>
+                        columns={columns}
+                        dataSource={data}
+                        style={{ width: "90%", height: "100%" }}
+                    />
+                </Flex>
+                <Modal
+                    title={editOrNew ? "编辑" : "新增"}
+                    open={isModalOpen}
+                    centered={true}
+                    onCancel={closeEditModal}
+                    onOk={handleSubmit}
+                >
+                    <Flex vertical gap="middle">
+                        <Flex gap="middle">
+                            <Flex flex={1} align="center" justify="center">
+                                <Flex flex={3}>日期：</Flex>
+                                <DatePicker
+                                    value={dayjs(updateData.date)}
+                                    showTime={{ format: "HH:mm:ss" }}
+                                    format="YYYY-MM-DD HH:mm:ss"
+                                    onChange={(_, value) =>
+                                        handleUpdataDataChange(
+                                            "date",
+                                            value as string,
+                                        )
+                                    }
+                                    style={{ flex: 8 }}
+                                ></DatePicker>
+                            </Flex>
+                            <Flex flex={1} align="center" justify="center">
+                                <Flex flex={2}>类别：</Flex>
+                                <AutoComplete
+                                    style={{ flex: 8 }}
+                                    onChange={(value) =>
+                                        handleUpdataDataChange(
+                                            "category",
+                                            value,
+                                        )
+                                    }
+                                    options={getOptions()}
+                                    allowClear={true}
+                                    backfill={true}
+                                    value={updateData.category as ""}
+                                    status={
+                                        updateData.category == undefined ||
+                                        updateData.category.length == 0
+                                            ? "error"
+                                            : ""
+                                    }
+                                />
+                            </Flex>
+                        </Flex>
+                        <Flex gap="middle">
+                            <Flex flex={1} align="center" justify="center">
+                                <Flex flex={3}>金额：</Flex>
+                                <InputNumber
+                                    style={{ flex: 8 }}
+                                    suffix="￥"
+                                    value={updateData.amount}
+                                    onChange={(value) =>
+                                        handleUpdataDataChange("amount", value)
+                                    }
+                                    precision={3}
+                                    status={
+                                        Number(updateData.amount) == 0
+                                            ? "error"
+                                            : ""
+                                    }
+                                ></InputNumber>
+                            </Flex>
+                            <Flex flex={1} align="center" justify="center">
+                                <Flex flex={3}>备注：</Flex>
+                                <Input
+                                    style={{ flex: 8 }}
+                                    onChange={(element) =>
+                                        handleUpdataDataChange(
+                                            "description",
+                                            element.target.value,
+                                        )
+                                    }
+                                    allowClear={true}
+                                    value={updateData.description}
+                                />
+                            </Flex>
+                        </Flex>
+                    </Flex>
+                </Modal>
+            </Flex>
+        </>
     );
 }
